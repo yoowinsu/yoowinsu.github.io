b66524b46cb77418822532f086482be28ba2654c
yoowinsu <suyingwei03@163.com>
2017-09-26 08:50:40 +0800

    Site updated: 2017-09-26 08:50:39

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Mon Sep 25 2017 18:54:35 GMT+0800 (中国标准时间)
+// build time:Tue Sep 26 2017 08:50:28 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
