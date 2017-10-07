6cac608e229f0b6c0c012e90bf6a69bb88bf3c6a
yoowinsu <suyingwei03@163.com>
2017-10-07 11:06:28 +0800

    Site updated: 2017-10-07 11:06:26

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Sun Oct 01 2017 13:22:51 GMT+0800 (中国标准时间)
+// build time:Sat Oct 07 2017 11:06:16 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
