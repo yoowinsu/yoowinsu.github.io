087d78e5e39602d4d730687407dab20cc80ee799
yoowinsu <suyingwei03@163.com>
2017-10-14 08:12:06 +0800

    Site updated: 2017-10-14 08:12:04

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Thu Oct 12 2017 18:54:21 GMT+0800 (中国标准时间)
+// build time:Sat Oct 14 2017 08:11:53 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
