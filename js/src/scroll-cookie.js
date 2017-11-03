395f768857160586507ebbdc95f080ee2159915a
yoowinsu <suyingwei03@163.com>
2017-11-03 17:53:20 +0800

    Site updated: 2017-11-03 17:53:18

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Nov 03 2017 16:54:43 GMT+0800 (中国标准时间)
+// build time:Fri Nov 03 2017 17:53:06 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
